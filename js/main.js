65fd9cfdc5c27c5d89dfc0edeba431472b82c954
Ha <leha525870@gmail.com>
2022-08-18 13:28:27 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+$(window).on("scroll", function () {
+    var scroll = $(window).scrollTop();
+    
+    if (scroll >280) {
+        $("#site-header").addClass("nav-fixed");
+    } else {
+        $("#site-header").removeClass("nav-fixed");
+    }
+});
